4388f32c57134221587cc8218e494eaeca9a48cc
Hieu Nv <hieunv.brainos@gmail.com>
2020-01-16 10:25:31 +0700

    add files source

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vietnamese(Tiếng Việt) Magento2 EE Language Pack (vi_VN)
+This translation is usefull for people living in the Vietnam (Việt Nam).
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'ecomteckm2eelanguage_vi_vn',
+    __DIR__
+);
